09545a2fda63a881e8460e9f60e977da1f783364
Kieran-777 <kieranadamsullivan@gmail.com>
2023-11-07 08:55:40 +0000

    cleaned up code working with stepper

--- laserHarpArduino/laserHarpArduino.ino
+++ laserHarpArduino/laserHarpArduino.ino
@@ -7,7 +7,7 @@ int delayLaser = 5;                     // If you increase this, the laser will 
 int delayMotor = 3;                     // This variable affects the speed, and fluidity of the harp.
 
 int beamSteps = 1;                      // Number of steps to take
-int stepSpeed = 10;                     // Stepper motor speed
+int stepSpeed = 200;                     // Stepper motor speed
 
 const int stepsPerRevolution = 2038;    // Defines the number of steps per rotation
 
@@ -25,9 +25,9 @@ int note1 = 0x47;
 
 int a, b, c, d, e, f, g, h, i = 0;       // Iniating the note status markers.
 
-const int NOTE_COUNT = 8;
-int* noteStatuses[NOTE_COUNT] = {&a, &b, &c, &d, &e, &f, &g, &h};
-int notes[NOTE_COUNT] = {note1, note2, note3, note4, note5, note6, note7, note8};
+const int beamCount = 8;
+int* noteStatuses[beamCount] = {&a, &b, &c, &d, &e, &f, &g, &h};
+int notes[beamCount] = {note1, note2, note3, note4, note5, note6, note7, note8};
 
 
 void setup() {
@@ -44,7 +44,7 @@ void noteOn(int cmd, int pitch, int velocity)     // Function to play the notes
 	Serial.write(velocity);
 }
 
-void beamBlock(int note, int &noteStatus)
+void beamBlock(int note, int &noteStatus, int pol)
 {
 	digitalWrite(LaserPin, HIGH);               // Turn on the laser for the 1st beam.
 	delay(delayLaser);
@@ -64,7 +64,7 @@ void beamBlock(int note, int &noteStatus)
 	digitalWrite(LaserPin, LOW);                // Turn off the Laser.
 
   myStepper.setSpeed(stepSpeed);              // Set stepper speed
-  myStepper.step(beamSteps);                  // Move stepper
+  myStepper.step(beamSteps * pol);                  // Move stepper
   delay(delayMotor);   
 }
 
@@ -72,61 +72,13 @@ void beamBlock(int note, int &noteStatus)
 
 void loop() {
 
-// So the whole pattern goes all the way left then all the way back again, bounces rather than loops
-// 1-2-3-4-5-6-7-8-7-6-5-4-3-2-end of loop so back to 1
-//                                        STANDARD BLOCK
-//--------------------------------------------------------------------------------------------------
-// 	digitalWrite(LaserPin, HIGH);               // Turn on the laser for the 1st beam.
-// 	delay(delayLaser);
-
-// 	if ( (analogRead(0) > sensor ) && (a == 0) ) // If the sensor gets a signal, and the note is not playing:
-// 	{
-// 		digitalWrite(13, HIGH);                   // Switch on status led.
-// 		noteOn(0x90, note1, 0x7F);                // Play note 1
-// 		a++;                                      // Change the status variable to one.
-// 	}
-// 	else if(analogRead(0) < sensor )            // If the sensor does not get a signal:
-// 	{
-// 		digitalWrite(13, LOW);                    // Switch off the status led.
-// 		noteOn(0x90, note1, 0x00);                // Stop playing note 1.
-// 		a = 0;                                    // Change the status variable to zero.
-// 	}
-// 	digitalWrite(LaserPin, LOW);                // Turn off the Laser.
-
-//   myStepper.setSpeed(stepSpeed);              // Set stepper speed
-//   myStepper.step(beamSteps);                  // Move stepper
-//   delay(delayMotor);                          // Pause briefly
-// //--------------------------------------------------------------------------------------------------
-
-
-// // supercool function version
-//   beamBlock(note1, a);
-//   beamBlock(note2, b);
-//   beamBlock(note3, c);
-//   beamBlock(note4, d);
-//   beamBlock(note5, e);
-//   beamBlock(note6, f);
-//   beamBlock(note7, g);
-//   beamBlock(note8, h);
-//   beamBlock(note7, g);
-//   beamBlock(note6, f);
-//   beamBlock(note5, e);
-//   beamBlock(note4, d);
-//   beamBlock(note3, c);
-//   beamBlock(note2, b);
-
-
-
-// even cooler looped version
-
   // Accesding
-  for (int i = 0; i < NOTE_COUNT; i++) {
-    beamBlock(notes[i], *noteStatuses[i]);
+  for (int i = 0; i < beamCount; i++) {
+    beamBlock(notes[i], *noteStatuses[i], 1);
   }
   // Decending
-  for (int i = NOTE_COUNT - 1; i >=0; i--) {
-    beamBlock(notes[i], *noteStatuses[i]);
+  for (int i = beamCount - 1; i >=0; i--) {
+    beamBlock(notes[i], *noteStatuses[i], -1);
   }
-
 
 }
