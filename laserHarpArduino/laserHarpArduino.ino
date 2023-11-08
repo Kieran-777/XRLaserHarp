92a6e129de0a38654902a7128f239feb574ab374
Kieran-777 <kieranadamsullivan@gmail.com>
2023-11-08 12:13:03 +0000

    Pre dymanic beamCount

--- laserHarpArduino/laserHarpArduino.ino
+++ laserHarpArduino/laserHarpArduino.ino
@@ -4,10 +4,10 @@ int LaserState = LOW;                    // The variable that stores the state o
 int LaserPin =  7;                       // Tell the arduino that the laser is on pin 7
 int sensor = 8 ;                         // Change this value to calibrate your harp's sensor
 int delayLaser = 5;                     // If you increase this, the laser will be brighter, but the harp will be less fluid
-int delayMotor = 3;                     // This variable affects the speed, and fluidity of the harp.
+int delayMotor = 200;                     // This variable affects the speed, and fluidity of the harp.
 
-int beamSteps = 1;                      // Number of steps to take
-int stepSpeed = 200;                     // Stepper motor speed
+int beamSteps = 20;                      // Number of steps to take
+int stepSpeed = 10;                     // Stepper motor speed
 
 const int stepsPerRevolution = 2038;    // Defines the number of steps per rotation
 
@@ -25,7 +25,7 @@ int note1 = 0x47;
 
 int a, b, c, d, e, f, g, h, i = 0;       // Iniating the note status markers.
 
-const int beamCount = 8;
+const int beamCount = 10;
 int* noteStatuses[beamCount] = {&a, &b, &c, &d, &e, &f, &g, &h};
 int notes[beamCount] = {note1, note2, note3, note4, note5, note6, note7, note8};
 
@@ -37,12 +37,14 @@ void setup() {
 
 }
 
+
 void noteOn(int cmd, int pitch, int velocity)     // Function to play the notes
 {
 	Serial.write(cmd);
 	Serial.write(pitch);
 	Serial.write(velocity);
 }
+
 
 void beamBlock(int note, int &noteStatus, int pol)
 {
@@ -52,14 +54,15 @@ void beamBlock(int note, int &noteStatus, int pol)
 	if ( (analogRead(0) > sensor ) && (noteStatus == 0) ) // If the sensor gets a signal, and the note is not playing:
 	{
 		digitalWrite(13, HIGH);                   // Switch on status led.
-		noteOn(0x90, note, 0x7F);                 // Play note 1
-		noteStatus++;                             // Change the status variable to one.
+		 noteOn(0x90, note, 0x7F);                 // Play note 1
+		 noteStatus++;                             // Change the status variable to one.
+
 	}
 	else if(analogRead(0) < sensor )            // If the sensor does not get a signal:
 	{
 		digitalWrite(13, LOW);                    // Switch off the status led.
-		noteOn(0x90, note, 0x00);                 // Stop playing note 1.
-		noteStatus = 0;                                    // Change the status variable to zero.
+		 noteOn(0x90, note, 0x00);                 // Stop playing note 1.
+		 noteStatus = 0;                                    // Change the status variable to zero.
 	}
 	digitalWrite(LaserPin, LOW);                // Turn off the Laser.
 
@@ -69,16 +72,15 @@ void beamBlock(int note, int &noteStatus, int pol)
 }
 
 
-
 void loop() {
 
   // Accesding
-  for (int i = 0; i < beamCount; i++) {
-    beamBlock(notes[i], *noteStatuses[i], 1);
+  for (int beam = 0; beam < beamCount; beam++) {
+    beamBlock(notes[beam], *noteStatuses[beam], 1);
   }
   // Decending
-  for (int i = beamCount - 1; i >=0; i--) {
-    beamBlock(notes[i], *noteStatuses[i], -1);
+  for (int beam = beamCount - 1; beam >=0; beam--) {
+    beamBlock(notes[beam], *noteStatuses[beam], -1);
   }
 
 }
