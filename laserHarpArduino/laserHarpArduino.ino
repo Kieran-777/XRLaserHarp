651e3cc57a470c83bad1731b4f18af99e168ba46
Kieran-777 <kieranadamsullivan@gmail.com>
2023-11-08 14:37:36 +0000

    dynamic beam count almost done, need to fix double bottom and top issue

--- laserHarpArduino/laserHarpArduino.ino
+++ laserHarpArduino/laserHarpArduino.ino
@@ -13,6 +13,7 @@ const int stepsPerRevolution = 2038;    // Defines the number of steps per rotat
 
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11); // Assign pins to stepper
 
+int note10 = 0x60;
 int note9 = 0x61;
 int note8 = 0x63;
 int note7 = 0x64;
@@ -23,17 +24,17 @@ int note3 = 0x71;
 int note2 = 0x40;
 int note1 = 0x47;
 
-int a, b, c, d, e, f, g, h, i = 0;       // Iniating the note status markers.
+int a, b, c, d, e, f, g, h, i, j = 0;       // Iniating the note status markers.
 
 const int beamCount = 10;
-int* noteStatuses[beamCount] = {&a, &b, &c, &d, &e, &f, &g, &h};
-int notes[beamCount] = {note1, note2, note3, note4, note5, note6, note7, note8};
+int* noteStatuses[beamCount] = {&a, &b, &c, &d, &e, &f, &g, &h, &i, &j};
+int notes[beamCount] = {note1, note2, note3, note4, note5, note6, note7, note8, note9, note10};
 
 
 void setup() {
 	pinMode(LaserPin, OUTPUT);            // Setup for laser.
 	pinMode(13, OUTPUT);                 // Setup for status led.
-	Serial.begin(31250);                 //  Start a serial communication channel for MIDI
+	Serial.begin(9600);                 //  Start a serial communication channel for MIDI
 
 }
 
@@ -48,21 +49,21 @@ void noteOn(int cmd, int pitch, int velocity)     // Function to play the notes
 
 void beamBlock(int note, int &noteStatus, int pol)
 {
-	digitalWrite(LaserPin, HIGH);               // Turn on the laser for the 1st beam.
+	digitalWrite(LaserPin, HIGH);               // Turn on the laser
 	delay(delayLaser);
 
 	if ( (analogRead(0) > sensor ) && (noteStatus == 0) ) // If the sensor gets a signal, and the note is not playing:
 	{
 		digitalWrite(13, HIGH);                   // Switch on status led.
-		 noteOn(0x90, note, 0x7F);                 // Play note 1
-		 noteStatus++;                             // Change the status variable to one.
+		//  noteOn(0x90, note, 0x7F);                 // Play note 1
+		//  noteStatus++;                             // Change the status variable to one.
 
 	}
 	else if(analogRead(0) < sensor )            // If the sensor does not get a signal:
 	{
 		digitalWrite(13, LOW);                    // Switch off the status led.
-		 noteOn(0x90, note, 0x00);                 // Stop playing note 1.
-		 noteStatus = 0;                                    // Change the status variable to zero.
+		//  noteOn(0x90, note, 0x00);                 // Stop playing note 1.
+		//  noteStatus = 0;                                    // Change the status variable to zero.
 	}
 	digitalWrite(LaserPin, LOW);                // Turn off the Laser.
 
@@ -71,16 +72,49 @@ void beamBlock(int note, int &noteStatus, int pol)
   delay(delayMotor);   
 }
 
+  int dynBeam = 9;
 
 void loop() {
+  
+  // Check if there's any Serial input
+  if (Serial.available() > 0) {
+    char touchBeam = Serial.read();
+    // Assuming the input is a single digit followed by newline
+    if (touchBeam >= '0' && touchBeam <= '9') {
+      dynBeam = touchBeam - '0';
+    }
+  }
+
+ 
 
   // Accesding
-  for (int beam = 0; beam < beamCount; beam++) {
-    beamBlock(notes[beam], *noteStatuses[beam], 1);
+  for (int beam = 0; beam < dynBeam; beam++) {
+  Serial.print("beam ");
+  Serial.print(beam);
+  Serial.println(" active");
+  beamBlock(notes[beam], *noteStatuses[beam], 1);
+
+    char touchBeam = Serial.read();
+    // Assuming the input is a single digit followed by newline
+    if (touchBeam >= '0' && touchBeam <= '9') {
+      dynBeam = touchBeam - '0';
+    }
+
   }
+
   // Decending
-  for (int beam = beamCount - 1; beam >=0; beam--) {
-    beamBlock(notes[beam], *noteStatuses[beam], -1);
+  for (int beam = dynBeam - 1; beam >= 0; beam--) {
+  Serial.print("beam ");
+  Serial.print(beam);
+  Serial.println(" active");
+  beamBlock(notes[beam], *noteStatuses[beam], -1);
+
+        char touchBeam = Serial.read();
+    // Assuming the input is a single digit followed by newline
+    if (touchBeam >= '0' && touchBeam <= '9') {
+      dynBeam = touchBeam - '0';
+    }
+
   }
 
 }
